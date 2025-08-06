ad489f89bd97a6bbd97fe2f8eebdf5546fa46bc9
falasyam <fala.syam@gmail.com>
2025-08-06 10:09:27 +0700

    Add: About Page, Logo SVG Component

---
+++ pages/about/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex container mx-auto max-w-4xl">
+    <NavBar />
+  </div>
+  <div class="py-12"></div>
+  <section>
+    <div class="container py-4 md:py-8 px-6 max-w-4xl mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <div class="font-bold text-2xl">Who We Are?</div>
+          <div class="pt-4">
+            CV SABILAJATI MEBEL JEPARA was established in 2021, we are a furniture manufacture located in Jepara, Central Java - Indonesia.
+          </div>
+        </div>
+        <div class="">
+          <TestLogo />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
