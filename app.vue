c8eb3012651f82047cf9e85ec4ba08865c468b5a
falasyam <fala.syam@gmail.com>
2026-01-28 15:00:12 +0700

    Change: Favicon

--- app.vue
+++ app.vue
@@ -15,7 +15,8 @@ useHead({
     {
       rel: 'stylesheet',
       href: 'https://fonts.bunny.net/css?family=plus-jakarta-sans:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i&display=swap'
-    }
+    },
+    { rel: "icon", href: "/favicon.ico" },
   ],
   htmlAttrs: {
     lang: locale,
